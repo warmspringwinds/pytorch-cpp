e2e6cb40fb3b093adc08bab296c48c812c3aa61d
Daniil Pakhomov <warmspringwinds@gmail.com>
2017-08-17 21:15:11 -0700

    torch::load() function to read hdf5 into map<string, Tensor> was added

--- examples/sequential_test.cpp
+++ examples/sequential_test.cpp
@@ -1251,7 +1251,7 @@ namespace torch
 
     }
 
-    std::vector<string> get_hdf5_dataset_keys(string hdf5_filename)
+    vector<string> get_hdf5_dataset_keys(string hdf5_filename)
     {
 
       // We open and close hdf5 file here. It might be an overkill
@@ -1262,13 +1262,13 @@ namespace torch
       // Open the file
       H5::H5File file = H5::H5File(hdf5_filename, H5F_ACC_RDONLY);
 
-      std::vector<string> names;
+      vector<string> names;
 
       // Define a closure to populate our names array
       auto closure = [] (hid_t loc_id, const char *name, const H5L_info_t *linfo, void *opdata) 
       {
 
-        std::vector<string> * names_array_pointer = reinterpret_cast< std::vector<string> *>(opdata);
+        vector<string> * names_array_pointer = reinterpret_cast< vector<string> *>(opdata);
 
         names_array_pointer->push_back(string(name));
 
@@ -1282,6 +1282,95 @@ namespace torch
 
       return names;
 
+    }
+
+    map<string, Tensor> load(string hdf5_filename)
+    {
+
+      map<string, Tensor> tensor_dict;
+
+      // use our get_names function
+      vector<string> tensor_names = get_hdf5_dataset_keys(hdf5_filename);
+
+      H5::H5File file = H5::H5File(hdf5_filename, H5F_ACC_RDONLY);
+
+      // Array to store the shape of the current tensor
+      hsize_t * dims_hsize_t;
+
+      // We need this because one function can't accept hsize_t
+      vector<long> dims_int;
+
+      // Float buffer to intermediately store weights
+      float * float_buffer;
+
+      // 'Rank' of the tensor
+      int ndims;
+
+      // Number of elements in the current tensor
+      hsize_t tensor_flattened_size;
+
+      Tensor buffer_tensor;
+
+
+      for (auto tensor_name: tensor_names)
+      {
+
+        dims_int.clear();
+
+        // Open a 'dataset' which stores current tensor
+        H5::DataSet current_dataset = file.openDataSet(tensor_name);
+
+        // We can infer the sizes of a store tensor from H5::DataSpace
+        H5::DataSpace dataspace = current_dataset.getSpace();
+        ndims = dataspace.getSimpleExtentNdims();
+
+        // Get the overall number of elements -- we need this
+        // to allocate the temporary buffer
+        tensor_flattened_size = dataspace.getSimpleExtentNpoints();
+
+        // Get the shame of the tensor
+        dims_hsize_t = new hsize_t[ndims];
+        dataspace.getSimpleExtentDims(dims_hsize_t, NULL);
+
+        for (int i = 0; i < ndims; ++i)
+        {
+
+          // Converting hsize_t to int
+          dims_int.push_back(long(dims_hsize_t[i]));
+        }
+
+        // Allocate temporary float buffer
+        // TODO: add support for other types like int
+        // and make automatic type inference
+        float_buffer = new float[tensor_flattened_size];
+
+        current_dataset.read(float_buffer, H5::PredType::NATIVE_FLOAT,
+                             dataspace, dataspace);
+
+
+        buffer_tensor = CPU(kFloat).tensorFromBlob(float_buffer, dims_int);
+
+        tensor_dict[tensor_name] = buffer_tensor.type().copy(buffer_tensor);
+
+        delete[] float_buffer;
+        delete[] dims_hsize_t;
+
+      }
+
+      file.close();
+
+      return tensor_dict;
+    }
+
+    void inspect_checkpoint(string hdf5_filename)
+    {
+
+      auto dict = load(hdf5_filename);
+
+      for (auto name_tensor_pair : dict)
+      {
+        cout << name_tensor_pair.first << ": " << name_tensor_pair.second.sizes() <<endl;
+      }
     }
 
 
@@ -1334,12 +1423,6 @@ int main()
   //  6) clean up the code
 
 
-
-  
-
-
-
-
   // auto net = torch::resnet18(21,    /* pascal # of classes */
   //                            true,  /* fully convolutional model */
   //                            8,     /* we want subsampled by 8 prediction*/
@@ -1387,64 +1470,12 @@ int main()
 
 
 
+
+
     // argument
     string hdf5_filename = "resnet18.h5";
 
-
-    std::vector<string> names = torch::get_hdf5_dataset_keys(hdf5_filename);
-
-    for(auto name: names)
-    {
-
-      std::cout << name << std::endl;
-    }
-
-
-    // // Array to store the shape of the current tensor
-    // hsize_t * dims;
-    // // Float buffer to intermediately store weights
-    // float * float_buffer;
-    // // 'Rank' of the tensor
-    // int ndims;
-    // // Number of elements in the current tensor
-    // hsize_t tensor_flattened_size;
-
-    // for (auto name_tensor_pair : dict)
-    // {
-
-    //   H5::DataSet dset = file.openDataSet(name_tensor_pair.first);
-
-    //   // Get the number of dimensions for the current tensor
-    //   H5::DataSpace dspace = dset.getSpace();
-    //   ndims = dspace.getSimpleExtentNdims();
-    //   tensor_flattened_size = dspace.getSimpleExtentNpoints();
-    //   dims = new hsize_t[ndims];
-    //   dspace.getSimpleExtentDims(dims, NULL);
-    //   float_buffer = new float[tensor_flattened_size];
-    //   H5::DataSpace mspace(ndims, dims);
-    //   dset.read(float_buffer, H5::PredType::NATIVE_FLOAT, mspace, 
-    //   dspace);
-
-    //   // Reading the raw floats into the Tensor
-    //   // But the buffer is not owned by Tensor -- we still need to free it up
-    //   // afterwards
-    //   auto buffer_tensor = CPU(kFloat).tensorFromBlob(float_buffer, name_tensor_pair.second.sizes());
-
-    //   name_tensor_pair.second.copy_(buffer_tensor);
-
-    //   delete[] dims;
-    //   delete[] float_buffer;
-    // }
-
-    //file.close();
-
-  
-  // open file -- get all the dataset names
-
-
-
-
-
+    torch::inspect_checkpoint(hdf5_filename);
 
   
   return 0;
